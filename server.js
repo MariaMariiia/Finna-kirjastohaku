47cbd094de6adbfa8ab3a894ce9aa1c5ad63e3c2
MariaMariiia <koskemar@yahoo.com>
2021-06-10 15:38:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ $('#form').on('submit',function(e){
 
 //Jos kieli on suomi ja aineistotyyppi on kirja:
 function getFinnishBooks() {
-  axios.get('https://finna.fi/Search/Results?limit=0&filter%5B%5D=%7Eformat%3A%220%2FBook%2F%22&filter%5B%5D=%7Elanguage%3A%22fin%22&type=AllFields')
+  axios.get('https://finna.fi/AJAX/JSON?filter%5B%5D=%7Elanguage%3A%22fin%22&filter%5B%5D=%7Eformat%3A%220%2FBook%2F%22&join=AND&bool0%5B%5D=AND&lookfor0%5B%5D=&type0%5B%5D=AllFields&method=getSideFacets&searchClassId=Solr&location=side&configIndex=0&query=filter%255B%255D%3D%257Elanguage%253A%2522fin%2522%26filter%255B%255D%3D%257Eformat%253A%25220%252FBook%252F%2522%26join%3DAND%26bool0%255B%255D%3DAND%26lookfor0%255B%255D%3D%26type0%255B%255D%3DAllFields&querySuppressed=0&extraFields=handler%2Climit%2CselectedShards%2Csort%2Cview&enabledFacets%5B%5D=free_online_boolean%3A1&enabledFacets%5B%5D=format_ext_str_mv')
    
 }
 
