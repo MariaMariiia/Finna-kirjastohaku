b25fe405187c84b9d431acff9de6f5be17cac3b2
MariaMariiia <koskemar@yahoo.com>
2021-06-14 14:35:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/Search/Results?sort=relevance&bool0[]=AND&lookfor0[]=&type0[]=AllFields&lookfor0[]=&type0[]=AllFields&join=AND&filter[]=~language%3A"fin"&filter[]=~format%3A"0%2FBook%2F"&limit=20', (req, res) => {
-  request(
-    { url: 'https://finna.fi/Search/Results?sort=relevance&bool0%5B%5D=AND&lookfor0%5B%5D=&type0%5B%5D=AllFields&lookfor0%5B%5D=&type0%5B%5D=AllFields&join=AND&filter%5B%5D=%7Elanguage%3A%22fin%22&filter%5B%5D=%7Eformat%3A%220%2FBook%2F%22&limit=20' },
+app.get('https://finna.fi/cache/ee6ade86faf34bb81b781594f0dbed69.min.js')
+    { url: 'https://finna.fi/cache/ee6ade86faf34bb81b781594f0dbed69.min.js' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
